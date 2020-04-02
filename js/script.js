b35dd5da5b8aa5887278d799c107c6c5a9b68cfb
Rose Kairu <rosekairu@gmail.com>
2020-04-02 23:07:20 +0300

    modify css stle rules

--- js/script.js
+++ js/script.js
@@ -14,13 +14,11 @@ function akanName() {
     var day = document.getElementById("day").value;
     var dd = parseInt(day);
     var cc = (yy - 1) / 100 + 1;
-    var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kweku", "Yaw", "Kofi", "Kwame"];
+    var maleName = ["Kwesi", "Kojo", "Kwabena", "Kweku", "Yaw", "Kofi", "Kwame"];
     var femaleName = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
     var result =
-        parseInt(cc / 4 - 2 * cc - 1 + (5 * yy) / 4 + (26 * (mm + 1)) / 10 + dd) %
-        7;
-
+        parseInt( (cc / 4) - 2 * cc - 1) + ((5 * yy / 4) + ((26 * (mm + 1) / 10)) + dd) % 7;
 
 
     if (document.getElementById("gender").checked) {
@@ -28,20 +26,20 @@ function akanName() {
     } else {
         var gender = "female";
     }
-    if (mm < 1 || mm > 12 || (mm == 2 && dd > 29)) {
-        alert("invalid month");
-    } else if (dd < 1 || dd > 31) {
-        alert("invalid date");
-    } else if ((mm === "April" || mm === "June" || mm === "September" || mm === "November") && (dd <= 0 || dd > 29)) {
-        alert("invalid Month");
+    if (mm < 1 || mm > 12) {
+        alert("Invalid Month");
+    } else if ((mm == 2 && dd > 29) && (0 == yy % 4)) {
+        alert("Please Enter a Valid Date!")
+    } else if ((mm == 2 && dd > 28) && (0 != yy % 4)) {
+        alert("Please Enter a Valid Date or Year! The Year Entered Was Not A Leap Year")
+    } else if ((mm == 1 || mm == 3 || mm == 5 || mm == 7 || mm == 8 || mm == 10 || mm == 12 ) && (dd < 1 || dd > 31)) {
+        alert("Invalid Date!");
+    } else if ((mm == 4 || mm == 6 || mm == 9 || mm == 11 ) && (dd < 1 || dd > 30)) {
+        alert("Invalid Date. This Month Has Only 30 Days" );
     } else if (mm === "------") {
-        alert("Please select a month!")
-    } else if (yy=="" || isNaN(yy) || yy.length!==4 && yy<= 1000 || yy > 10000) {
-        alert("Please enter a valid year!")
-    } else if ((mm === "February") && (dd <= 0 || dd > 29) && (0 == yy % 4)) {
-        alert("Please enter a valid date!")
-    } else if ((mm === "February") && (dd <= 0 || dd > 28) && (0 != yy % 4)) {
-        alert("Please enter a valid date!")
+        alert("Please Select A Month!")
+    } else if (yy=="" || isNaN(yy) || yy.length!==4 && yy < 1000 || yy > 10000) {
+        alert("Please Enter A Valid Year!")
     } else if (Math.round(result) == 0 && gender === "male") {
         image('malesunday.jpg');
         document.getElementById("results").innerHTML =
