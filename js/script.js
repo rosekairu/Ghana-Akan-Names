263496f78fbd172db277a44a27ca6dc4e57f6be6
Rose Kairu <rosekairu@gmail.com>
2020-04-05 23:06:27 +0300

    modify js file

--- js/script.js
+++ js/script.js
@@ -16,19 +16,21 @@ function akanName() {
     var day = document.getElementById("day").value;
     var dd = parseInt(day);
     var cc = (yy - 1) / 100 + 1;
+    var dateOfBirth = new Date;
+    var dayOfTheWeek = dateOfBirth.getDay();
     var maleName = ["Kwesi", "Kojo/Kwadwo", "Kwabena", "Kweku", "Yaw", "Kofi", "Kwame"];
     var femaleName = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afia/Afua", "Ama"];
 
-    var weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
+    var weekDay = new Array(7);
+    weekDay[0] = "Sunday";
+    weekDay[1] = "Monday";
+    weekDay[2] = "Tuesday";
+    weekDay[3] = "Wednesday";
+    weekDay[4] = "Thursday";
+    weekDay[5] = "Friday";
+    weekDay[6] = "Saturday";
 
-    var dayOfTheWeek = (cc / 4 - 2 * cc - 1 + (5 * yy) / 4 + (26 * (mm + 1)) / 10 + dd) % 7;
+    dayOfTheWeek = (cc / 4 - 2 * cc - 1 + (5 * yy) / 4 + (26 * (mm + 1)) / 10 + dd) % 7;
 
     dayOfTheWeek = parseInt(dayOfTheWeek);
 
@@ -54,59 +56,59 @@ function akanName() {
     } else if (dayOfTheWeek == 0 && gender === "male") {
         image('malesunday.jpg');
         document.getElementById("results").innerHTML =
-            "You were born on " + weekday[0] + ". " + "Your Akan name is " + maleName[0];
+            "You were born on " + weekDay[0] + ". " + "Your Akan name is " + maleName[0];
     } else if (dayOfTheWeek == 0 && gender === "female") {
         image('femalesunday.jpg');
         document.getElementById("results").innerHTML =
-            "You were born on " + weekday[0] + ". " + "Your Akan name is " + " " + femaleName[0];
+            "You were born on " + weekDay[0] + ". " + "Your Akan name is " + " " + femaleName[0];
     } else if (dayOfTheWeek == 1 && gender === "male") {
         image('malemonday.jpg');
         document.getElementById("results").innerHTML =
-            "You were born on " + weekday[1] + ". " + "Your Akan name is " + maleName[1];
+            "You were born on " + weekDay[1] + ". " + "Your Akan name is " + maleName[1];
     } else if (dayOfTheWeek == 1 && gender === "female") {
         image('femalemonday.jpg');
         document.getElementById("results").innerHTML =
-            "You were born on " + weekday[1] + ". " + "Your Akan name is " + femaleName[1];
+            "You were born on " + weekDay[1] + ". " + "Your Akan name is " + femaleName[1];
     } else if (dayOfTheWeek == 2 && gender === "male") {
         image('maletuesday.jpg');
         document.getElementById("results").innerHTML =
-            "You were born on " + weekday[2] + ". " + "Your Akan name is " + maleName[2];
+            "You were born on " + weekDay[2] + ". " + "Your Akan name is " + maleName[2];
     } else if (dayOfTheWeek == 2 && gender === "female") {
         image('femaletuesday.jpg');
         document.getElementById("results").innerHTML =
-            "You were born on " + weekday[2] + ". " + "Your Akan name is " + femaleName[2];
+            "You were born on " + weekDay[2] + ". " + "Your Akan name is " + femaleName[2];
     } else if (dayOfTheWeek == 3 && gender === "male") {
         image('malewednesday.jpg');
         document.getElementById("results").innerHTML =
-            "You were born on " + weekday[3] + ". " + "Your Akan name is " + maleName[3];
+            "You were born on " + weekDay[3] + ". " + "Your Akan name is " + maleName[3];
     } else if (dayOfTheWeek == 3 && gender === "female") {
         image('femalewednesday.jpg');
         document.getElementById("results").innerHTML =
-            "You were born on " + weekday[3] + ". " + "Your Akan name is " + femaleName[3];
+            "You were born on " + weekDay[3] + ". " + "Your Akan name is " + femaleName[3];
     } else if (dayOfTheWeek == 4 && gender === "male") {
         image('malethursday.jpg');
         document.getElementById("results").innerHTML =
-            "You were born on " + weekday[4] + ". " + "Your Akan name is " + maleName[4];
+            "You were born on " + weekDay[4] + ". " + "Your Akan name is " + maleName[4];
     } else if (dayOfTheWeek == 4 && gender === "female") {
         image('femalethursday.jpg');
         document.getElementById("results").innerHTML =
-            "You were born on " + weekday[4] + ". " + "Your Akan name is " + femaleName[4];
+            "You were born on " + weekDay[4] + ". " + "Your Akan name is " + femaleName[4];
     } else if (dayOfTheWeek == 5 && gender === "male") {
         image('malefriday.jpg');
         document.getElementById("results").innerHTML =
-            "You were born on " + weekday[5] + ". " + "Your Akan name is " + maleName[5];
+            "You were born on " + weekDay[5] + ". " + "Your Akan name is " + maleName[5];
     } else if (dayOfTheWeek == 5 && gender === "female") {
         image('femalefriday.jpg');
         document.getElementById("results").innerHTML =
-            "You were born on " + weekday[5] + ". " + "Your Akan name is " + femaleName[5];
+            "You were born on " + weekDay[5] + ". " + "Your Akan name is " + femaleName[5];
     } else if (dayOfTheWeek == 6 && gender === "male") {
         image('malesaturday.jpg');
         document.getElementById("results").innerHTML =
-            "You were born on " + weekday[6] + ". " + "Your Akan name is " + maleName[6];
+            "You were born on " + weekDay[6] + ". " + "Your Akan name is " + maleName[6];
     } else if (dayOfTheWeek == 6 && gender === "female") {
         image('femalesaturday.jpg');
         document.getElementById("results").innerHTML =
-            "You were born on " + weekday[6] + ". " + "Your Akan ame is " + femaleName[6];
+            "You were born on " + weekDay[6] + ". " + "Your Akan ame is " + femaleName[6];
     } else {
         alert("Fill in all required fields");
     }
